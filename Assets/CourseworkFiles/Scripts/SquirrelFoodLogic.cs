32b6b0767ef9b75d78276c85a981e8a8fbfc5ca1
Jason Lavarinhas <kayloshai@gmail.com>
2020-01-10 14:06:09 +0000

    Created new scene
    Created my own scene under coursework in assets folder

---
+++ Assets/CourseworkFiles/Scripts/SquirrelFoodLogic.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SquirrelFoodLogic : MonoBehaviour
+{
+    public bool respawn;
+    public SquirrelArea myArea;
+
+    public void OnEaten()
+    {
+        if (respawn)
+        {
+            transform.position = new Vector3(Random.Range(-myArea.range, myArea.range),
+                3f,
+                Random.Range(-myArea.range, myArea.range)) + myArea.transform.position;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
